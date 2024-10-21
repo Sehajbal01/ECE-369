d9a8a8ffb785b049bccc1f37b9fd615b03029188
jtg38 <jtg3@arizona.edu>
2024-10-21 16:04:19 -0700

    Update Controller.v

--- Controller.v
+++ Controller.v
@@ -1,26 +1,65 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/10/2024 11:17:14 AM
-// Design Name: 
-// Module Name: Controller
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead,
+Branch,MemToReg,Jump, ALUOp);
 
-module Controller(
-
-    );
+    input [31:0] Instruction;
+    
+    output reg RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Branch, MemToReg, Jump;
+    output reg [1:0]ALUOp;
+    
+    always@(Instruction) begin
+    case(Instruction[31:26])
+    6'b000000 : begin // R-type instructions
+    ALUSrc <= 0;
+    RegDst <= 1;
+    MemWrite <= 0;
+    MemRead <= 0;
+    Branch <= 0;
+    MemToReg <= 1;
+    Jump <= 0;
+    RegWrite <= 1;
+    ALUOp <= 10;
+    end
+    
+    6'b000100: begin
+    ALUSrc <= 0;
+    RegDst <= 0;
+    MemWrite <= 0;
+    MemRead <= 0;
+    Branch <= 1;
+    MemToReg <= 1;
+    Jump <= 0;
+    RegWrite <= 1;
+    ALUOp <= 01;
+    end
+    
+    6'b100011: begin
+    ALUSrc <= 1;
+    RegDst <= 0;
+    MemWrite <= 0;
+    MemRead <= 1;
+    Branch <= 0;
+    MemToReg <= 1;
+    Jump <= 0;
+    RegWrite <= 1;
+    ALUOp <= 00;
+    end
+    
+    6'b101011: begin
+    ALUSrc <= 1;
+    RegDst <= 0;
+    MemWrite <= 1;
+    MemRead <= 0;
+    Branch <= 0;
+    MemToReg <= 1;
+    Jump <= 0;
+    RegWrite <= 1;
+    ALUOp <= 00;
+    end
+    
+    endcase
+    end
+    
 endmodule
+    
